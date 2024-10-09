ecfd1bc66b838f80731ddea4695dbb5466806a42
Chris White <142789525+chris-white-wtw@users.noreply.github.com>
2024-10-09 15:22:49 +0100

    Example of strange WinRT code generation in SDK 8.0.403

---
+++ Objects/DisposableAction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Objects
+{
+  /// <summary>
+  /// Creates a class that calls the specified action in its Dispose method. 
+  /// Typically used to replace
+  /// try
+  /// {
+  /// }
+  /// finally
+  /// {
+  ///   Cleanup();
+  /// }
+  /// 
+  /// with
+  /// using (new DisposableAction(() => Cleanup())
+  /// {
+  /// }
+  /// </summary>
+  public class DisposableAction : IDisposable
+  {
+    private Action mDisposeAction;
+    public DisposableAction(Action disposeAction)
+    {
+      mDisposeAction = disposeAction;
+    }
+
+    public void Dispose()
+    {
+      if (mDisposeAction == null)
+        return;
+      Action disposeAction = mDisposeAction;
+      mDisposeAction = null;
+      disposeAction();
+    }
+  }
+}
---
+++ test/ProgressExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Objects;
+
+namespace Progress
+{
+  public static class ProgressExtensions
+  {
+    private static Action<SystemException> sHandleProgressException;
+
+    /// <remarks>Only intended for use in tests.</remarks>
+    public static IDisposable WithExceptionHandler(Action<SystemException> handler)
+    {
+      Action<SystemException> prev;
+      (sHandleProgressException, prev) = (handler, sHandleProgressException);
+      return new DisposableAction(() => sHandleProgressException = prev);
+    }
+ }
+}
